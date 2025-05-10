34777199da79d3b45429de74490ef7982540b397
박현우 <simuoo96@gmail.com>
2025-05-10 16:40:36 +0900

    feat : splash화면 구현

---
+++ android/app/src/main/kotlin/com/example/chrc/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.chrc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/Splash/splash_screen.dart
@@ -0,0 +1,56 @@
+import 'package:chrc/main.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() => runApp(Splash());
+
+class Splash extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  void initState() {
+    super.initState();
+    _init();
+  }
+
+  void _init() async {
+    await Future.delayed(Duration(seconds: 3));
+    Navigator.pushReplacement(
+        context,
+    MaterialPageRoute(builder: (context) => MyApp())
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/images/church_logo.png',
+              fit: BoxFit.fill,
+            ),
+            Text("\n"),
+            CircularProgressIndicator(color: Colors.black12,),
+          ],
+        ),
+      ),
+    );
+  }
+}
